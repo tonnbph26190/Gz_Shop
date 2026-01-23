ae3532c8e03963d5e15c7e6546b7ec56398f3f10
NGUYENNHATLINH <linh.tayyo.2005@gmail.com>
2026-01-23 20:28:39 +0700

    done linhnnph

---
+++ QuanView/Areas/Admin/Controllers/BaoCaoThongKeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuanView.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin")] // Route tùy chỉnh để truy cập qua /Admin/ThongKe
+    public class BaoCaoThongKeController : Controller
+    {
+        [HttpGet("ThongKe")]
+        public IActionResult ThongKe()
+        {
+            // Gọi chính xác view ở Areas/Admin/Views/BaoCaoThongKe/ThongKe.cshtml
+            return View("~/Areas/Admin/Views/BaoCaoThongKe/ThongKe.cshtml");
+        }
+    }
+}
