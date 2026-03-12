6c2c35e9716500411216ce32fd3eae8289508833
ARAKUNA-MARCUS <tinhlvph49319@gmail.com>
2026-03-12 21:32:53 +0700

    fix

--- QuanApi/Controllers/KhachHangController.cs
+++ QuanApi/Controllers/KhachHangController.cs
@@ -246,15 +246,7 @@ namespace QuanApi.Controllers.Api
 
                     emailBody.AppendLine("<p>Cảm ơn bạn đã sử dụng dịch vụ của chúng tôi!</p>");
                     emailBody.AppendLine("<p>Trân trọng,</p>");
-<<<<<<< HEAD
                     emailBody.AppendLine("<p><strong>Cửa hàng bán quần âu GZ</strong></p>");
-=======
-<<<<<<< HEAD
-                    emailBody.AppendLine("<p><strong>Cửa hàng bán quần âu GZ</strong></p>");
-=======
-                    emailBody.AppendLine("<p><strong>Cửa hàng bán quần áo GZ</strong></p>");
->>>>>>> big_update
->>>>>>> visual
 
                     try
                     {
