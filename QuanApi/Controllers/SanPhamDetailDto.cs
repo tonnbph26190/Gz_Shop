ebc746eec1e9c4f00965070f0c566603500c0b18
adm <adm@HIEUPHAN>
2026-05-13 21:02:21 +0700

    done

--- QuanApi/Controllers/SanPhamDetailDto.cs
+++ QuanApi/Controllers/SanPhamDetailDto.cs
@@ -7,7 +7,7 @@ public class SanPhamDetailDto
 	public Guid IdSanPham { get; set; }
 	public string TenSanPham { get; set; }
 	public string TenDanhMuc { get; set; }
-
+	public bool TrangThai { get; set; }
 	public List<string> DanhSachAnh { get; set; } = new();
 
 	public List<SanPhamChiTietDto> BienThes { get; set; } = new();
